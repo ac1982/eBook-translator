fa0c683cf71ec74fe0a988df96ea48be7d279240
AC <alexever@gmail.com>
2023-05-11 23:09:19 +0800

    much more comments for codes

--- translator.py
+++ translator.py
@@ -12,9 +12,12 @@ import concurrent
 # 加载配置文件
 with open("config.json", "r") as f:
     config = json.load(f)
+# 使用"config.json"文件打开一个文件对象，并使用json.load()方法加载配置文件内容到config变量中
 
 # 根据配置文件中的模型名称设置 MAX_TOKENS
 model_name = config['model']
+# 从配置文件中获取模型名称并存储到model_name变量中
+
 if model_name == "gpt-4":
     MAX_TOKENS = 2400
 elif model_name == "gpt-3.5-turbo":
@@ -22,21 +25,38 @@ elif model_name == "gpt-3.5-turbo":
 else:
     print(f"未知模型: {model_name}")
     sys.exit(1)
+# 根据模型名称设置最大令牌数(MAX_TOKENS)。如果模型名称不在已知的模型列表中，打印错误消息并退出程序。
 
 ENCODING_NAME = "cl100k_base"
+# 设置编码名称为"cl100k_base"
+
 THRESHOLD = MAX_TOKENS/2
+# 设置阈值为最大令牌数的一半
+
 total_tokens = 0
+# 初始化总令牌数为0
+
 max_workers = config.get('max_workers', None)
+# 从配置文件中获取最大工作线程数，如果配置文件中没有指定，则为None
 
 if max_workers is None:
     max_workers = os.cpu_count()
-
+# 如果最大工作线程数未指定，则将其设置为可用的CPU核心数(os.cpu_count()返回CPU核心数)
 
 def num_tokens_from_string(string: str) -> int:
     """Returns the number of tokens in a text string."""
+    # 函数声明，接受一个名为string的字符串参数，返回一个整数类型的值
+
     encoding = tiktoken.encoding_for_model(config['model'])
+    # 根据配置文件中的模型名称获取编码器
+    # 使用模型名称从`tiktoken`模块中获取相应的编码器
+
     num_tokens = len(encoding.encode(string))
+    # 使用编码器对文本字符串进行编码，并计算编码后的令牌数量
+    # 通过对编码后的字符串返回的列表进行长度计算得到令牌数量
+
     return num_tokens
+    # 返回令牌数量作为函数的结果
 
 # 使用递归的方法翻译超过MAX_TOKENS的内容
 def translate_recursive(soup, level=1):
@@ -60,9 +80,15 @@ def translate_recursive(soup, level=1):
         print(f"递归层数超过{level}层，停止程序。")
         sys.exit(1)
 
-    for i,child in enumerate(children):  # 遍历子节点
+    for i, child in enumerate(children):  # 遍历子节点
         child_html = str(child)  # 将子节点转换为 HTML 字符串
+        child_html = child_html.strip()  # 去除前导和尾随空格
+
+        if not child_html:  # 如果child_html为空字符串
+            continue  # 跳过当前循环迭代
+
         child_tokens = num_tokens_from_string(child_html)  # 计算子节点的 tokens 数量
+        # 继续处理子节点的令牌数量...
         if child_tokens < MAX_TOKENS:
             # 如果子节点的 token 数量大于 THRESHOLD，先清空缓冲区，然后直接处理 child_html
             if child_tokens >= THRESHOLD:
@@ -158,75 +184,93 @@ def translate_item(content):
 
 if __name__ == '__main__':
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
-    
+    # 创建一个线程池执行器，用于并行执行任务
+
     parser = argparse.ArgumentParser(description='Translate an EPUB file.')
+    # 创建一个参数解析器，用于解析命令行参数
     parser.add_argument('input_file', type=str,
                         help='The path to the input EPUB file.')
+    # 添加一个位置参数，表示输入的EPUB文件的路径
 
     args = parser.parse_args()
+    # 解析命令行参数并存储到args变量中
+
     try:
         book = epub.read_epub(args.input_file)
     except FileNotFoundError:
         print(f"文件 '{args.input_file}' 不存在，请检查文件名和路径是否正确。")
         sys.exit(1)
+    # 尝试读取输入的EPUB文件，并将其存储到book变量中
+    # 如果文件不存在，则打印错误消息并退出程序
 
     new_book = epub.EpubBook()
+    # 创建一个新的EpubBook对象，用于存储翻译后的电子书
 
     dc_keys = ['identifier', 'title', 'language', 'creator', 'contributor',
                'publisher', 'rights', 'coverage', 'date', 'description']
+    # 定义一个包含Dublin Core元数据关键字的列表
+
     for key in dc_keys:
         metadata = book.get_metadata('DC', key)
+        # 获取原始电子书中特定关键字的Dublin Core元数据
         for entry in metadata:
             new_book.add_metadata('DC', key, entry[0], others=entry[1])
+        # 将Dublin Core元数据添加到新的电子书中
 
     custom_metadata = book.get_metadata('OPF', None)
+    # 获取原始电子书中的自定义元数据
     for entry in custom_metadata:
         if 'name' in entry[1] and 'content' in entry[1]:
             new_book.add_metadata(
                 'OPF', entry[1]['name'], entry[1]['content'], others=entry[1])
-
-    item_count = 0
-    items = list(book.get_items())
-    total_items = len(items)
+        # 将自定义元数据添加到新的电子书中
     
-    item_results = {}
-    item_futures = []
-    total_tokens = 0
+    item_count = 0  # 初始化项目计数为0
+    items = list(book.get_items())  # 获取原始电子书中的所有项目，并将其转换为列表
+    total_items = len(items)  # 获取项目的总数
+
+    item_results = {}  # 创建一个空的字典，用于存储处理结果
+    item_futures = []  # 创建一个空列表，用于存储任务的未来对象
+    total_tokens = 0  # 记录总的令牌数量
 
     for index, item in enumerate(items):
-        if item.get_type() == ebooklib.ITEM_DOCUMENT:
-            item_count += 1
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:  # 检查项目类型是否为文档
+            item_count += 1  # 项目计数加1
             if config['test'] and item_count > max(1, total_items // 10):
-                #测试环境只加载1/10的ITEMS
-                break
+                # 在测试环境下，只加载1/10的项目
+                break  # 如果项目数量超过指定阈值，则跳出循环
 
-            original_content = item.get_content().decode('utf-8')
-            future = executor.submit(translate_item, original_content)
-            item_futures.append((index, item, future))
+            original_content = item.get_content().decode('utf-8')  # 获取原始内容并解码为UTF-8格式
+            future = executor.submit(translate_item, original_content)  # 提交翻译任务给执行器，获得一个未来对象
+            item_futures.append((index, item, future))  # 将索引、项目和未来对象添加到列表中
 
     for index, item, future in item_futures:
-        new_content, item_cost_tokens = future.result()
-        total_tokens += item_cost_tokens
-        item_results[index] = (new_content, item_cost_tokens)
+        new_content, item_cost_tokens = future.result()  # 获取未来对象的结果（阻塞，直到结果可用）
+        total_tokens += item_cost_tokens  # 增加令牌数量
+        item_results[index] = (new_content, item_cost_tokens)  # 将处理结果存储到字典中
 
     for index, item in enumerate(items):
-        if index in item_results:
-            new_content, _ = item_results[index]
+        if index in item_results:  # 检查处理结果中是否存在当前索引
+            new_content, _ = item_results[index]  # 获取处理结果中的新内容
             new_item = epub.EpubItem(uid=item.id, file_name=item.file_name,
-                                     media_type=item.media_type, content=new_content)
-            new_book.add_item(new_item)
-            # Update TOC
+                                    media_type=item.media_type, content=new_content)  # 创建一个新的EpubItem对象
+            new_book.add_item(new_item)  # 将新的项目添加到新的电子书中
+            # 更新目录
             for toc_entry in new_book.toc:
                 if toc_entry.href == item.file_name:
-                    toc_entry.item = new_item
+                    toc_entry.item = new_item  # 更新目录中的项目引用
         else:
-            new_book.add_item(item)
+            new_book.add_item(item)  # 如果处理结果中不存在当前索引，则将原始项目添加到新的电子书中
 
-    new_book.toc = book.toc
-    new_book.spine = book.spine
-    new_book.guide = book.guide
+    new_book.toc = book.toc  # 将原始电子书的目录复制到新的电子书中
+    new_book.spine = book.spine  # 将原始电子书的内容顺序复制到新的电子书中
+    new_book.guide = book.guide  # 将原始电子书的引导信息复制到新的电子书中
 
     output_file = args.input_file.split('.')[0] + '_zh.epub'
+    # 根据输入文件名生成输出文件名，使用.split('.')获取文件名部分（不含扩展名），然后添加'_zh.epub'后缀
     epub.write_epub(output_file, new_book)
+    # 将新的电子书写入输出文件，使用生成的输出文件名和新的电子书对象作为参数
     usd_dollar = (total_tokens/1000)*0.002
+    # 计算令牌成本，将总令牌数量除以1000，然后乘以0.002（假设成本为每千个令牌的0.002 USD）
     print(f"Cost tokens: {usd_dollar:.2f} USD")
+    # 打印令牌成本，保留两位小数，打印格式为"{usd_dollar:.2f} USD"
