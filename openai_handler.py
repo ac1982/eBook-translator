a29f9d9148a767547fbcd100c85e9153ea5ec612
AC <alexever@gmail.com>
2023-05-11 23:46:27 +0800

    format with black

--- openai_handler.py
+++ openai_handler.py
@@ -1,32 +1,41 @@
 import openai
 import json
-from tenacity import retry, wait_exponential, stop_after_attempt, retry_if_exception_type
+from tenacity import (
+    retry,
+    wait_exponential,
+    stop_after_attempt,
+    retry_if_exception_type,
+)
 from requests.exceptions import SSLError
 from openai.error import RateLimitError, APIConnectionError, APIError
+
 
 # 使用 tenacity 重试装饰器
 @retry(
     wait=wait_exponential(multiplier=1, max=180),
     stop=stop_after_attempt(10),
-    retry=retry_if_exception_type((RateLimitError, APIConnectionError, APIError, SSLError)),
+    retry=retry_if_exception_type(
+        (RateLimitError, APIConnectionError, APIError, SSLError)
+    ),
 )
 def completion_with_backoff(**kwargs):
     return openai.ChatCompletion.create(**kwargs)
 
+
 # 定义一个翻译函数
 def translate_content(content):
     # 从配置文件中读取配置
-    with open('config.json', 'r') as f:
+    with open("config.json", "r") as f:
         config = json.load(f)
 
     # 设置 OpenAI API 密钥
-    openai.api_key = config['openai_api_key']
+    openai.api_key = config["openai_api_key"]
 
     # 调用具有指数退避策略的聊天补全函数
     response = completion_with_backoff(
-        model=config['model'],
+        model=config["model"],
         messages=[
-            {"role": "system", "content": config['system']},
+            {"role": "system", "content": config["system"]},
             {"role": "user", "content": content},
         ],
         temperature=0.3,  # 调整这个值以控制随机性
@@ -35,5 +44,5 @@ def translate_content(content):
 
     # 提取翻译后的文本和使用的总 token 数
     translated_text = response.choices[0].message.content
-    total_tokens = response.usage['total_tokens']
+    total_tokens = response.usage["total_tokens"]
     return translated_text, total_tokens
--- translator.py
+++ translator.py
@@ -14,8 +14,7 @@ with open("config.json", "r") as f:
     config = json.load(f)
 # 使用"config.json"文件打开一个文件对象，并使用json.load()方法加载配置文件内容到config变量中
 
-# 根据配置文件中的模型名称设置 MAX_TOKENS
-model_name = config['model']
+model_name = config["model"]
 # 从配置文件中获取模型名称并存储到model_name变量中
 
 if model_name == "gpt-4":
@@ -30,24 +29,25 @@ else:
 ENCODING_NAME = "cl100k_base"
 # 设置编码名称为"cl100k_base"
 
-THRESHOLD = MAX_TOKENS/2
+THRESHOLD = MAX_TOKENS / 2
 # 设置阈值为最大令牌数的一半
 
 total_tokens = 0
 # 初始化总令牌数为0
 
-max_workers = config.get('max_workers', None)
+max_workers = config.get("max_workers", None)
 # 从配置文件中获取最大工作线程数，如果配置文件中没有指定，则为None
 
 if max_workers is None:
     max_workers = os.cpu_count()
 # 如果最大工作线程数未指定，则将其设置为可用的CPU核心数(os.cpu_count()返回CPU核心数)
 
+
 def num_tokens_from_string(string: str) -> int:
     """Returns the number of tokens in a text string."""
     # 函数声明，接受一个名为string的字符串参数，返回一个整数类型的值
 
-    encoding = tiktoken.encoding_for_model(config['model'])
+    encoding = tiktoken.encoding_for_model(config["model"])
     # 根据配置文件中的模型名称获取编码器
     # 使用模型名称从`tiktoken`模块中获取相应的编码器
 
@@ -58,21 +58,22 @@ def num_tokens_from_string(string: str) -> int:
     return num_tokens
     # 返回令牌数量作为函数的结果
 
+
 # 使用递归的方法翻译超过MAX_TOKENS的内容
 def translate_recursive(soup, level=1):
     # 检查 soup 是否为空
     if not soup:
         # 如果为空,返回空字符串和0
-        return '', 0
+        return "", 0
 
     children = list(soup.children)  # 获取 soup 的子节点列表
     children_number = len(children)  # 计算子节点的数量
-    if config['test']:
+    if config["test"]:
         print(f"Level {level} 的子节点(Children)数量{children_number}")
 
-    translated_content = ''  # 初始化翻译后的内容字符串
+    translated_content = ""  # 初始化翻译后的内容字符串
     cost_tokens = 0  # 初始化已用 tokens 数量
-    buffer = ''  # 初始化缓冲区字符串
+    buffer = ""  # 初始化缓冲区字符串
     buffer_tokens = 0  # 初始化缓冲区 tokens 数量
 
     # 避免错误导致的无限递归
@@ -93,17 +94,20 @@ def translate_recursive(soup, level=1):
             # 如果子节点的 token 数量大于 THRESHOLD，先清空缓冲区，然后直接处理 child_html
             if child_tokens >= THRESHOLD:
                 if buffer:
-                    if config['test']:
+                    if config["test"]:
                         print(
-                            f"Level:{level} 第{i+1} 子节点 > THRESHOLD\nBuffer不为空，Buffer的Tokens：{buffer_tokens}\n翻译Buffer内容: \n{buffer}\n")
+                            f"Level:{level} 第{i+1} 子节点 > THRESHOLD\nBuffer不为空，Buffer的Tokens：{buffer_tokens}\n翻译Buffer内容: \n{buffer}\n"
+                        )
                     translated_buffer, buffer_cost_tokens = translate_content(buffer)
                     translated_content += translated_buffer
                     cost_tokens += buffer_cost_tokens
-                    buffer = ''
+                    buffer = ""
                     buffer_tokens = 0
 
-                if config['test']:
-                    print(f"该Level:{level} 第{i+1} 子节点 Tokens：{child_tokens} < THRESHOLD 直接处理")
+                if config["test"]:
+                    print(
+                        f"该Level:{level} 第{i+1} 子节点 Tokens：{child_tokens} < THRESHOLD 直接处理"
+                    )
                 translated_child, child_cost_tokens = translate_content(child_html)
                 translated_content += translated_child
                 cost_tokens += child_cost_tokens
@@ -115,23 +119,26 @@ def translate_recursive(soup, level=1):
                 buffer_tokens += child_tokens
                 # 如果添加后缓冲区超过THRESHOLD，那么清空缓冲区
                 if buffer_tokens >= THRESHOLD:
-                    if config['test']:
+                    if config["test"]:
                         print(
-                            f"该Level:{level} 第{i+1} 子节点,把其添加到的Buffer后，Tokens超过Threshold。\nTokens：{buffer_tokens} \n翻译Buffer内容: \n{buffer}\n")
+                            f"该Level:{level} 第{i+1} 子节点,把其添加到的Buffer后，Tokens超过Threshold。\nTokens：{buffer_tokens} \n翻译Buffer内容: \n{buffer}\n"
+                        )
                     translated_buffer, buffer_cost_tokens = translate_content(buffer)
                     translated_content += translated_buffer
                     cost_tokens += buffer_cost_tokens
-                    buffer = ''
+                    buffer = ""
                     buffer_tokens = 0
         else:
             # 先清空缓冲区
             if buffer:
-                if config['test']:
-                    print(f"该Level:{level} 第{i+1} 子节点Tokens：{buffer_tokens} > MAX_TOKENS \n清空Buffer后递归处理,Buffer: \n{buffer}\n")
+                if config["test"]:
+                    print(
+                        f"该Level:{level} 第{i+1} 子节点Tokens：{buffer_tokens} > MAX_TOKENS \n清空Buffer后递归处理,Buffer: \n{buffer}\n"
+                    )
                 translated_buffer, buffer_cost_tokens = translate_content(buffer)
                 translated_content += translated_buffer
                 cost_tokens += buffer_cost_tokens
-                buffer = ''
+                buffer = ""
                 buffer_tokens = 0
 
             # 递归处理子节点
@@ -141,12 +148,14 @@ def translate_recursive(soup, level=1):
 
     # 遍历结束，处理剩余的Buffer
     if buffer:
-        if config['test']:
-            print(f"遍历Level:{level}结束，缓冲区仍有Buffer\nBuffer Tokens：{buffer_tokens} \n翻译Buffer:\n{buffer}\n")
+        if config["test"]:
+            print(
+                f"遍历Level:{level}结束，缓冲区仍有Buffer\nBuffer Tokens：{buffer_tokens} \n翻译Buffer:\n{buffer}\n"
+            )
         translated_buffer, buffer_cost_tokens = translate_content(buffer)
         translated_content += translated_buffer
         cost_tokens += buffer_cost_tokens
- 
+
     return translated_content, cost_tokens  # 返回翻译后的内容和已用 tokens 数量
 
 
@@ -154,39 +163,40 @@ def translate_recursive(soup, level=1):
 def translate_item(content):
     # global total_tokens  # 使用全局变量 total_tokens 来跟踪已翻译的 tokens 数量
     count = num_tokens_from_string(content)  # 计算输入内容的 tokens 数量
-    item_cost_tokens=0
-    if config['test']:
+    item_cost_tokens = 0
+    if config["test"]:
         print(f"该 ITEM 的tokens合计：{count}")
 
     if count < MAX_TOKENS:
         # 如果输入内容的 tokens 数量小于 MAX_TOKENS
-        if config['test']:
+        if config["test"]:
             print("Translating the entire content.\n")
         new_item_content, cost_tokens = translate_content(content)  # 直接翻译整个内容
         item_cost_tokens += cost_tokens  # 累加已用 tokens 数量
     else:
         # 如果输入内容的 tokens 数量大于 MAX_TOKENS，需要逐部分翻译
-        if config['test']:
+        if config["test"]:
             print("Translating the content by parts.\n")
-        soup = BeautifulSoup(content, 'html.parser')  # 使用 BeautifulSoup 解析 HTML 内容
+        # 使用 BeautifulSoup 解析 HTML 内容
+        soup = BeautifulSoup(content, "html.parser")
         translated_body, cost_tokens = translate_recursive(soup.body)  # 递归地翻译子元素
         item_cost_tokens += cost_tokens  # 累加已用 tokens 数量
         # 将翻译后的 body 内容替换原始 soup 对象中的 body 内容
         soup.body.clear()  # 清空原始 soup 对象中的 body 内容
-        soup.body.append(BeautifulSoup(translated_body, 'html.parser'))  # 将翻译后的内容添加到 soup 的 body 中
+        # 将翻译后的内容添加到 soup 的 body 中
+        soup.body.append(BeautifulSoup(translated_body, "html.parser"))
         new_item_content = str(soup)  # 获取整个 HTML 字符串（包括翻译后的内容）
 
     return new_item_content, item_cost_tokens  # 返回翻译后的内容
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
     # 创建一个线程池执行器，用于并行执行任务
 
-    parser = argparse.ArgumentParser(description='Translate an EPUB file.')
+    parser = argparse.ArgumentParser(description="Translate an EPUB file.")
     # 创建一个参数解析器，用于解析命令行参数
-    parser.add_argument('input_file', type=str,
-                        help='The path to the input EPUB file.')
+    parser.add_argument("input_file", type=str, help="The path to the input EPUB file.")
     # 添加一个位置参数，表示输入的EPUB文件的路径
 
     args = parser.parse_args()
@@ -203,25 +213,36 @@ if __name__ == '__main__':
     new_book = epub.EpubBook()
     # 创建一个新的EpubBook对象，用于存储翻译后的电子书
 
-    dc_keys = ['identifier', 'title', 'language', 'creator', 'contributor',
-               'publisher', 'rights', 'coverage', 'date', 'description']
+    dc_keys = [
+        "identifier",
+        "title",
+        "language",
+        "creator",
+        "contributor",
+        "publisher",
+        "rights",
+        "coverage",
+        "date",
+        "description",
+    ]
     # 定义一个包含Dublin Core元数据关键字的列表
 
     for key in dc_keys:
-        metadata = book.get_metadata('DC', key)
+        metadata = book.get_metadata("DC", key)
         # 获取原始电子书中特定关键字的Dublin Core元数据
         for entry in metadata:
-            new_book.add_metadata('DC', key, entry[0], others=entry[1])
+            new_book.add_metadata("DC", key, entry[0], others=entry[1])
         # 将Dublin Core元数据添加到新的电子书中
 
-    custom_metadata = book.get_metadata('OPF', None)
+    custom_metadata = book.get_metadata("OPF", None)
     # 获取原始电子书中的自定义元数据
     for entry in custom_metadata:
-        if 'name' in entry[1] and 'content' in entry[1]:
+        if "name" in entry[1] and "content" in entry[1]:
             new_book.add_metadata(
-                'OPF', entry[1]['name'], entry[1]['content'], others=entry[1])
+                "OPF", entry[1]["name"], entry[1]["content"], others=entry[1]
+            )
         # 将自定义元数据添加到新的电子书中
-    
+
     item_count = 0  # 初始化项目计数为0
     items = list(book.get_items())  # 获取原始电子书中的所有项目，并将其转换为列表
     total_items = len(items)  # 获取项目的总数
@@ -233,12 +254,14 @@ if __name__ == '__main__':
     for index, item in enumerate(items):
         if item.get_type() == ebooklib.ITEM_DOCUMENT:  # 检查项目类型是否为文档
             item_count += 1  # 项目计数加1
-            if config['test'] and item_count > max(1, total_items // 10):
+            if config["test"] and item_count > max(1, total_items // 10):
                 # 在测试环境下，只加载1/10的项目
                 break  # 如果项目数量超过指定阈值，则跳出循环
 
-            original_content = item.get_content().decode('utf-8')  # 获取原始内容并解码为UTF-8格式
-            future = executor.submit(translate_item, original_content)  # 提交翻译任务给执行器，获得一个未来对象
+            original_content = item.get_content().decode("utf-8")  # 获取原始内容并解码为UTF-8格式
+            future = executor.submit(
+                translate_item, original_content
+            )  # 提交翻译任务给执行器，获得一个未来对象
             item_futures.append((index, item, future))  # 将索引、项目和未来对象添加到列表中
 
     for index, item, future in item_futures:
@@ -249,8 +272,12 @@ if __name__ == '__main__':
     for index, item in enumerate(items):
         if index in item_results:  # 检查处理结果中是否存在当前索引
             new_content, _ = item_results[index]  # 获取处理结果中的新内容
-            new_item = epub.EpubItem(uid=item.id, file_name=item.file_name,
-                                    media_type=item.media_type, content=new_content)  # 创建一个新的EpubItem对象
+            new_item = epub.EpubItem(
+                uid=item.id,
+                file_name=item.file_name,
+                media_type=item.media_type,
+                content=new_content,
+            )  # 创建一个新的EpubItem对象
             new_book.add_item(new_item)  # 将新的项目添加到新的电子书中
             # 更新目录
             for toc_entry in new_book.toc:
@@ -263,11 +290,11 @@ if __name__ == '__main__':
     new_book.spine = book.spine  # 将原始电子书的内容顺序复制到新的电子书中
     new_book.guide = book.guide  # 将原始电子书的引导信息复制到新的电子书中
 
-    output_file = args.input_file.split('.')[0] + '_zh.epub'
+    output_file = args.input_file.split(".")[0] + "_zh.epub"
     # 根据输入文件名生成输出文件名，使用.split('.')获取文件名部分（不含扩展名），然后添加'_zh.epub'后缀
     epub.write_epub(output_file, new_book)
     # 将新的电子书写入输出文件，使用生成的输出文件名和新的电子书对象作为参数
-    usd_dollar = (total_tokens/1000)*0.002
+    usd_dollar = (total_tokens / 1000) * 0.002
     # 计算令牌成本，将总令牌数量除以1000，然后乘以0.002（假设成本为每千个令牌的0.002 USD）
     print(f"Cost tokens: {usd_dollar:.2f} USD")
     # 打印令牌成本，保留两位小数，打印格式为"{usd_dollar:.2f} USD"
